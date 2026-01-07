84bd69824793be8c115575d6085128624830a9dd
The Captaan <ayushanand5143@gmail.com>
2026-01-07 17:20:09 +0530

    social link fixes

--- src/components/FloatingSocial.tsx
+++ src/components/FloatingSocial.tsx
@@ -8,10 +8,10 @@ export default function FloatingSocial() {
             <a href="https://www.facebook.com/profile.php?id=61585858032488" target="_blank" rel="noopener noreferrer">
                 <img src={FacebookIcon} className="social-icon" alt="" />
             </a>
-            <a href="https://wa.link/9njb0b" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.instagram.com/benneluru/" target="_blank" rel="noopener noreferrer">
                 <img src={InstagramIcon} className="social-icon" alt="" />
             </a>
-            <a href="https://www.instagram.com/benneluru/" target="_blank" rel="noopener noreferrer">
+            <a href="https://wa.link/9njb0b" target="_blank" rel="noopener noreferrer">
                 <img src={WhatsappIcon} className="social-icon" alt="" />
             </a>
         </div>
